87fc18398a84d833399c1ff0a3f48a5e0eda22fe
Hayden Wies <haydenwies@gmail.com>
2025-09-16 21:20:28 -0400

    added backend url to config

--- apps/backend/src/config/router.config.ts
+++ apps/backend/src/config/router.config.ts
@@ -2,10 +2,12 @@ import { registerAs } from "@nestjs/config"
 import z from "zod"
 
 const routerConfigSchema = z.object({
+	BACKEND_URL: z.url(),
 	WEB_URL: z.url()
 })
 
 const routerConfig = registerAs("router", () => ({
+	backendUrl: process.env.BACKEND_URL,
 	webUrl: process.env.WEB_URL
 }))
 
