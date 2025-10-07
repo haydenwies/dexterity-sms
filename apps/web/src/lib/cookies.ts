4993544faa5d4dab94be4956381078756d51a87e
Hayden Wies <haydenwies@gmail.com>
2025-10-08 00:41:22 +0200

    removed domain property in setCookie

--- apps/web/src/lib/cookies.ts
+++ apps/web/src/lib/cookies.ts
@@ -5,8 +5,7 @@ const setCookie = async (name: string, value: string): Promise<void> => {
 	cookieStore.set(name, value, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "lax",
-		domain: "railway.app"
+		sameSite: "lax"
 	})
 }
 
