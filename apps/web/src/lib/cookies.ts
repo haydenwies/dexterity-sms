171434fbdca492f80a12af7662ef0bb29eacf67b
Hayden Wies <haydenwies@gmail.com>
2025-10-08 00:28:48 +0200

    removed domain from cookie settings

--- apps/web/src/lib/cookies.ts
+++ apps/web/src/lib/cookies.ts
@@ -5,7 +5,8 @@ const setCookie = async (name: string, value: string): Promise<void> => {
 	cookieStore.set(name, value, {
 		httpOnly: true,
 		secure: process.env.NODE_ENV === "production",
-		sameSite: "lax"
+		sameSite: "lax",
+		domain: undefined
 	})
 }
 
