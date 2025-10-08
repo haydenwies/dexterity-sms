2a76b223f1a3b07fd9205e49defb7fb478851472
Hayden Wies <haydenwies@gmail.com>
2025-10-08 06:28:28 +0200

    added domain spec for cookies

--- apps/web/src/lib/cookies.ts
+++ apps/web/src/lib/cookies.ts
@@ -3,10 +3,13 @@ import { cookies } from "next/headers"
 const setCookie = async (name: string, value: string): Promise<void> => {
 	const cookieStore = await cookies()
 
+	const appEnv = process.env.APP_ENV || "development"
+
 	cookieStore.set(name, value, {
 		httpOnly: true,
-		secure: process.env.APP_ENV === "development" ? false : true,
-		sameSite: "none"
+		secure: appEnv !== "development",
+		sameSite: appEnv === "development" ? "lax" : "none",
+		domain: appEnv !== "development" ? ".bytte.io" : undefined
 	})
 }
 
